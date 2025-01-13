11eaf13531d065fa48e28e8b601d52222ae2c195
salcxjo <salar.miri@gmail.com>
2025-01-12 22:55:53 -0700

    fixed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="py-10 md:py-20 bg-white">
       <div className="max-w-6xl mx-auto px-4 md:px-8">
         <div className="flex flex-row justify-between items-center gap-8 md:gap-0 md:px-20 pr-3">
-          <div className="flex flex-col items-center justify-center md:items-start w-full md:w-auto">
+          <div className="flex flex-col items-center justify-center md:items-center w-full md:w-auto">
             <h3 className="text-xl font-semibold mb-2">Visit</h3>
             <a href="https://ca.linkedin.com/company/university-of-alberta-engineers-in-action" className="text-blue-600 mb-1 justify-center" target="_blank" rel="noopener noreferrer">Linkedin</a>
             <a href="https://www.instagram.com/uofaeia/?hl=en" className="text-blue-600 justify-center" target="_blank" rel="noopener noreferrer">Instagram</a>
